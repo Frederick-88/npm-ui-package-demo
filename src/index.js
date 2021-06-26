d8eb06af1168064380acd6687cd6a7ea3718ff7c
Chen Frederick <frederickfd88@gmail.com>
2021-06-26 21:22:47 +0700

    Update Documentation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import * as SampleUI from "./components";
 
-// create vue.component - prepare to be used globally
-// similar like after we import components, then in <script> we have "components: {component1, component2}"
+// loop to register Vue.component - prepare to be used globally
+
+// vue.component is similar to vue import components like :
+/*
+import AccordionItem from "./AccordionItem";
+
+export default {
+  name: "Accordion",
+  components: {
+    AccordionItem,
+  },
+*/
+// but defining Vue.component in the parent level, will make it able to be used in components without need to re-import / define
+
 const GlobalVue = {
   install(Vue) {
     Object.values(SampleUI).forEach((Component) => {
